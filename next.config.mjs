deaf648e742cd89d3f12d7c011fd3b8fa49280d4
Isaac Weber <isaac.weber@bambee.com>
2023-06-12 10:33:29 -0500

    Ignore errors

--- next.config.mjs
+++ next.config.mjs
@@ -30,5 +30,10 @@ const config = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 export default config;
